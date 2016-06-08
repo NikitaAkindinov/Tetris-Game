8cd97e8fc72adec83cbfa8fd6edc672f06007449
Nikita Akindinov <akindinov-n@yandex.com>
2016-06-08 18:36:04 +0300

    Библиотека

---
+++ Tetris/Elements.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    public class Elements
+    {
+        private static string emptySpace = "   ";// Символ наполнения пустого пространства.
+        private static string item = " * ";    // Символ отображения фигур.
+        int startDotWidth; // Начальная точка от которой будет вестись построение фигуры на поле. К ней же прибавляется смещение фигуры
+        int startDotHeight = 0;// Начальная точка от которой будет вестись построение фигуры на поле. К ней же прибавляется смещение фигуры
+        string[,] elementsArray;
+
+        #region//Свойства
+        public string[,] ElementsArray
+        {
+            get
+            {
+                return elementsArray;
+            }
+        }
+
+        public static string EmptySpace
+        {
+            get
+            {
+                return emptySpace;
+            }
+        }
+
+        public static string Item
+        {
+            get
+            {
+                return item;
+            }
+        }
+
+        public int StartDotHeight
+        {
+            get
+            {
+                return startDotHeight;
+            }
+
+            set
+            {
+                this.startDotHeight = value;
+            }
+        }
+
+        public int StartDotWidth
+        {
+            get
+            {
+                return startDotWidth;
+            }
+
+            set
+            {
+                this.startDotWidth = value;
+            }
+        }
+        #endregion
+
+        // Конструктор класса
+        public Elements(int numberElement)
+        {
+            /* Расшифровка номеров фигур
+             * 0 - Квадрат   * *
+             *              * *
+             * 1 - Зигзаг       * *
+             *                * * 
+             * 2 - Прямая   * * * * * *
+             * 3 - Уголок   * * * *
+             *              * 
+             * 4 - Уголок   * * * *
+             *                    * 
+             */
+
+            switch (numberElement)
+            {
+                case 0:
+                    {
+                        elementsArray = new string[,] { { Item, Item }, { Item, Item } };
+                        StartDotWidth = Field.Width / 2 - elementsArray.GetLength(1) / 2;
+                    }
+                    break;
+                case 1:
+                    {
+                        elementsArray = new string[,] { { EmptySpace, Item, Item }, { Item, Item, EmptySpace } };
+                        StartDotWidth = Field.Width / 2 - elementsArray.GetLength(1) / 2;
+                    }
+                    break;
+                case 2:
+                    {
+                        elementsArray = new string[,] { { Item, Item, Item, Item, Item }, { EmptySpace, EmptySpace, EmptySpace, EmptySpace, EmptySpace } };
+                        StartDotWidth = Field.Width / 2 - elementsArray.GetLength(1) / 2;
+                    }
+                    break;
+                case 3:
+                    {
+                        elementsArray = new string[,] { { Item, Item, Item, Item }, { Item, EmptySpace, EmptySpace, EmptySpace } };
+                        StartDotWidth = Field.Width / 2 - elementsArray.GetLength(1) / 2;
+                    }
+                    break;
+                case 4:
+                    {
+                        elementsArray = new string[,] { { Item, Item, Item, Item }, { EmptySpace, EmptySpace, EmptySpace, Item } };
+                        StartDotWidth = Field.Width / 2 - elementsArray.GetLength(1) / 2;
+                    }
+                    break;
+
+                default: break;
+            }
+        }
+
+        // Метод поворота фигуры
+        public void Left()
+        {
+            string[,] temp = new string[elementsArray.GetLength(1), elementsArray.GetLength(0)];
+
+            for (int i = 0, I = elementsArray.GetLength(0) - 1; i < elementsArray.GetLength(0); i++, I--)
+                for (int j = 0, J = elementsArray.GetLength(1) - 1; j < elementsArray.GetLength(1); j++, J--)
+                    temp[J, i] = elementsArray[i, j];
+
+            elementsArray = temp;
+        }
+        public void Right()
+        {
+            string[,] temp = new string[elementsArray.GetLength(1), elementsArray.GetLength(0)];
+
+            for (int i = 0, I = elementsArray.GetLength(0) - 1; i < elementsArray.GetLength(0); i++, I--)
+                for (int j = 0, J = elementsArray.GetLength(1) - 1; j < elementsArray.GetLength(1); j++, J--)
+                    temp[j, I] = elementsArray[i, j];
+
+            elementsArray = temp;
+        }
+
+    }
+}
--- Tetris/Field.cs
+++ Tetris/Field.cs
@@ -8,28 +8,150 @@ namespace Tetris
 {
     public class Field
     {
-        int height = 25;// Высота
-        int width = 10;// Ширина
-        string box = "###";// Рамка поля, обрамление.
-        string emptySpace = " ~ ";// Символ наполнения пустого пространства.
-        string item = "*";    // Символ отображения фигур.
-        string[,]  playingFieled;
+        static int height = 25;// Высота
+        static int width = 10;// Ширина
+        public string box = "###";// Рамка поля, обрамление.
+        public string[,] playingFieled = new string[Height, Width];
 
-        public Field(int i,int j)
+        public void ViewClearField()
         {
-            this.height = i;
-            this.width = j;
-            playingFieled = new string[i, j];
-            
+            for (int i = 0; i < playingFieled.GetLength(0); i++)// Перебор строк
+                for (int j = 0; j < playingFieled.GetLength(1); j++)//Перебор столбцов
+                    playingFieled[i, j] = Elements.EmptySpace;
         }
-        void bild()
-        {
-            for (int i = 0; i < height + 2;i++ )
-            {
-                for (int j = 0; j < width + 2; j++)
-                {
 
+        /*
+         * 0 - Всё хорошо
+         * 1 - Ошибка перемещения. Нужно создать новую фигуру
+         * 2 - Игра окончена
+         * */
+        public int Down(Elements elem)
+        {
+
+            if (elem.StartDotHeight == 0)
+            {
+                elem.StartDotHeight++;
+                if (!CheckOpenPlace(elem))
+                    return 2;
+            }
+            else
+            {
+                DeliteElemtnts(elem);
+                elem.StartDotHeight++;
+                if (!CheckOpenPlace(elem))
+                {
+                    elem.StartDotHeight--;
+                    BildElement(elem);
+                    return 1;
                 }
+            }
+            BildElement(elem);
+
+            return 0;
+        }
+        public int Turn(Elements elem)// Метод поворота фигуры
+        {
+            DeliteElemtnts(elem);
+            elem.Right();
+            if (!CheckOpenPlace(elem))
+            {
+                elem.Left();
+                BildElement(elem);
+                return 1;
+            }
+            BildElement(elem);
+            return 0;
+        }
+        bool CheckOpenPlace(Elements elem) // Готово. Работает
+        {
+            for (int i = 0; i < elem.ElementsArray.GetLength(0); i++)
+                for (int j = 0; j < elem.ElementsArray.GetLength(1); j++)
+                    if (elem.ElementsArray[i, j] == Elements.Item)
+                    {
+                        if (elem.StartDotHeight + i == playingFieled.GetLength(0))
+                            return false;
+
+                        if (elem.StartDotWidth + j == playingFieled.GetLength(1))
+                            return false;
+
+                        if (elem.StartDotWidth + j == -1)
+                            return false;
+
+                        if (playingFieled[elem.StartDotHeight + i, elem.StartDotWidth + j] == Elements.Item)
+                            return false;
+                    }
+            return true;
+        }
+
+        public int Left(Elements elem)
+        {
+            DeliteElemtnts(elem);
+            elem.StartDotWidth--;
+            if (!CheckOpenPlace(elem))
+            {
+                elem.StartDotWidth++;
+                BildElement(elem);
+                return 1;
+            }
+            BildElement(elem);
+
+            return 0;
+        }
+        public int Right(Elements elem)
+        {
+
+            DeliteElemtnts(elem);
+            elem.StartDotWidth++;
+            if (!CheckOpenPlace(elem))
+            {
+                elem.StartDotWidth--;
+                BildElement(elem);
+                return 1;
+            }
+            BildElement(elem);
+
+            return 0;
+        }
+
+        public void DeliteElemtnts(Elements elem)
+        {
+            for (int i = 0; i < elem.ElementsArray.GetLength(0); i++)
+                for (int j = 0; j < elem.ElementsArray.GetLength(1); j++)
+                    if (elem.ElementsArray[i, j] == Elements.Item)
+                        playingFieled[elem.StartDotHeight + i, elem.StartDotWidth + j] = Elements.EmptySpace;
+        }
+
+        public void BildElement(Elements elem)
+        {
+            for (int i = 0; i < elem.ElementsArray.GetLength(0); i++)
+                for (int j = 0; j < elem.ElementsArray.GetLength(1); j++)
+                    if (elem.ElementsArray[i, j] == Elements.Item)
+                        playingFieled[elem.StartDotHeight + i, elem.StartDotWidth + j] = elem.ElementsArray[i, j];
+        }
+
+        public static int Height
+        {
+            get
+            {
+                return height;
+            }
+
+            set
+            {
+                height = value;
+            }
+        }
+
+        public static int Width
+        {
+            get
+            {
+                return width;
+            }
+
+            set
+            {
+                width = value;
             }
         }
     }
